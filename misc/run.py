7ae348cac387583f0efe9be72ee427d90557ce85
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-08-05 11:53:43 +0900

    Update run.py (Refactoring inline test support)

--- misc/run.py
+++ misc/run.py
@@ -5,7 +5,7 @@ import argparse
 import io
 
 
-def find_tests(file):
+def find_inline_tests(file):
     with open(file) as f:
         s = f.read()
     ls = re.finditer("%%%% begin\n(.*?)%%%%\n(.*?)%%%% end", s, re.DOTALL)
@@ -13,7 +13,29 @@ def find_tests(file):
     return tests
 
 
-def run_cpp(file, option, check, test_infile, test_outfile, no_pch):
+def test_cpp(exec_file, name, inp, outp, check):
+    print(f":: Running test ({name})")
+    proc = subprocess.Popen(exec_file, stdin=PIPE, stdout=PIPE)
+    proc_stdout, proc_stderr = proc.communicate(input=bytes(inp, "utf-8"))
+    proc_outp = proc_stdout.decode()
+    if proc.returncode != 0:
+        print(f":: returncode: {proc.returncode}")  # e.g. -11 for segfault
+        print(proc_outp)
+        return
+    if not check:
+        print(proc_outp)
+        return
+    if proc_outp == outp:
+        print(":: Check success")
+        return
+    print(":: Check failed")
+    print(":::: EXPECTED")
+    print(outp)
+    print(":::: ACTUAL")
+    print(proc_outp)
+
+
+def run_cpp(file, option, check, test, no_pch):
     compiler = "clang++"
     default_option = "-std=c++17 -O2 -march=native -Wall -Wextra -Wshadow"
     exec_file = "./build/main"
@@ -36,37 +58,30 @@ def run_cpp(file, option, check, test_infile, test_outfile, no_pch):
     if proc.stdout:  # Compile warning if any
         print(proc.stdout.decode())
 
-    if test_infile:
-        print(f":: Running test ({test_infile})")
-        with open(test_infile) as f:
-            proc = subprocess.Popen(exec_file, stdin=f, stdout=PIPE)
-            proc_stdout, proc_stderr = proc.communicate()
-            proc_outp = proc_stdout.decode()
-            if not test_outfile:
-                print(proc_outp)
+    if test.startswith("inline:"):
+        test = test[len("infile:") :]
+        inline_tests = find_inline_tests(file)
+        if test == "all":
+            selected = list(range(len(inline_tests)))
+        else:
+            # e.g. inline:0,1,2
+            selected = [int(x) for x in test.split(",")]
+        print(f":: Run inline tests: {selected}")
+        for i, (inp, outp) in enumerate(inline_tests):
+            if not i in selected:
+                continue
+            test_cpp(exec_file, f"inline:{i}", inp, outp, check)
         return
 
-    tests = find_tests(file)
-    for i, (inp, outp) in enumerate(tests):
-        print(f":: Running test ({i + 1})")
-        proc = subprocess.Popen(exec_file, stdin=PIPE, stdout=PIPE)
-        proc_stdout, proc_stderr = proc.communicate(input=bytes(inp, "utf-8"))
-        if proc.returncode != 0:
-            print(f":: returncode: {proc.returncode}")  # e.g. -11 for segfault
-            print(proc_stdout.decode())
-            continue
-        proc_outp = proc_stdout.decode()
-        if not check:
-            print(proc_outp)
-            continue
-        if proc_outp == outp:
-            print(":: Check success")
-            continue
-        print(":: Check failed")
-        print(":::: EXPECTED")
-        print(outp)
-        print(":::: ACTUAL")
-        print(proc_outp)
+    if test.startswith("file:"):
+        test = test[len("file:") :]
+        infile, outfile = test.split(":") if ":" in test else [test, None]
+        inp = open(infile).read()
+        outp = open(outfile).read() if outfile else ""
+        test_cpp(exec_file, f"{infile}, {outfile}", inp, outp, check)
+        return
+
+    raise RuntimeError(f"Found unsupported test: {test}")
 
 
 def main():
@@ -74,8 +89,12 @@ def main():
     parser.add_argument("file", type=str)
     parser.add_argument("--option", type=str)
     parser.add_argument("--check", action="store_true", default=False)
-    parser.add_argument("--test-infile", type=str)
-    parser.add_argument("--test-outfile", type=str)
+    parser.add_argument(
+        "--test",
+        type=str,
+        default="inline:all",
+        help='e.g. "inline:all", "inline:0,1,2", "file:in.txt:out.txt"',
+    )
     parser.add_argument("--no-pch", action="store_true", default=False)
     args = parser.parse_args()
     run_cpp(**args.__dict__)
