be402626ebf49cb0efd9b4662cff59957d90b1a4
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-08-07 13:56:55 +0900

    Update misc/example.cpp

--- misc/example.cpp
+++ misc/example.cpp
@@ -1,5 +1,5 @@
 //
-// Current default header for C++
+// Default setup for C++
 //
 
 #include <bits/stdc++.h>
@@ -10,10 +10,42 @@ using namespace std;
 #define RANGE(I, X, Y) for (int I = (X); I < (Y); I++)
 using ll = int64_t;
 using ull = uint64_t;
+namespace std {
+template<class ...Ts> istream& operator>>(istream& i, tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class T>     istream& operator>>(istream& i, vector<T>& x)    { for (auto& y : x) { i >> y; } return i; }
+}
 
 // Debugging
 #define DD(X) std::cout << #X ": " << (X) << std::endl << std::flush
-template<class ...Ts> ostream&        operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
+namespace std {
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
 template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { o << tie(x.first, x.second); return o; }
-template<class T> ostream&            operator<<(ostream& o, const vector<T>& x)    { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+template<class T>            ostream& operator<<(ostream& o, const vector<T>& x)    { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+template<class T1, class T2> ostream& operator<<(ostream& o, const set<T1, T2>& x)  { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
 template<class T1, class T2> ostream& operator<<(ostream& o, const map<T1, T2>& x)  { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+}
+
+// Main
+int solve() {
+  return 0;
+}
+
+void mainCase() {
+  auto result = solve();
+  cout << result << endl;
+}
+
+int main() {
+  int t;
+  cin >> t;
+  RANGE(i, 0, t) mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py xxx/main.cpp --check
+
+%%%% begin
+%%%%
+%%%% end
+*/
