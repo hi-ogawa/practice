363331e04b2d6470e3a8b22a8e0827d904e03d90
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-08-22 19:31:59 +0900

    Update cses/tree_algorithms

---
+++ cses/tree_algorithms/task1130/main.cpp
@@ -0,0 +1,93 @@
+// AC
+
+// CLAIM. Existance of normal form of matching in tree (aka greedy)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utility
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+template<typename T> struct is_container {
+template<typename C> static char test(typename C::const_iterator*); template<typename> static int test(...);
+static bool const value = sizeof(test<T>(nullptr)) == sizeof(char) && !std::is_same<std::string, T>::value;
+};
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i, tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class T1, class T2> istream& operator>>(istream& i, pair<T1, T2>& x) { i >> x.first >> x.second; return i; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+istream& operator>>(istream& i, T& x) { for (auto& y : x) { i >> y; } return i; }
+}
+
+// Debugging
+#define DD(X) std::cout << #X ": " << (X) << std::endl << std::flush
+#define DD2(X) std::cout << #X ":" << std::endl; for (auto x : (X)) { std::cout << x << std::endl << std::flush; }
+namespace std {
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { o << tie(x.first, x.second); return o; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+}
+
+// Main
+void mainCase() {
+  int n; // <= 10^6
+  cin >> n;
+
+  vector<tuple<int, int>> edges(n - 1, {0, 0});
+  cin >> edges;
+
+  vector<vector<int>> adj(n);
+  for (auto [x, y] : edges) {
+    x--; y--;
+    adj[x].push_back(y); adj[y].push_back(x);
+  }
+  // DD(adj);
+
+  vector<int> res(n, -1);
+  function<int(int)> dfs = [&](int v) {
+    res[v] = 0;
+    // res[v] = 1 <=> ∀u. res[u] = 0;
+    int tmp = 1;
+    for (auto u : adj[v]) {
+      if (res[u] == -1) { tmp *= dfs(u); }
+    }
+    res[v] = !tmp;
+    return res[v];
+  };
+  FOR(v, 0, n) {
+    if (res[v] == -1) { dfs(v); }
+  }
+  // DD(res);
+
+  int res1 = accumulate(ALL(res), (int)0);
+  cout << res1 << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  // [ Single case ]
+  mainCase();
+  return 0;
+  // [ Multiple cases ]
+  // int t;
+  // cin >> t;
+  // FOR(i, 0, t) { mainCase(); }
+  // return 0;
+}
+
+/*
+python misc/run.py cses/tree_algorithms/task1130/main.cpp --check
+
+%%%% begin
+5
+1 2
+1 3
+3 4
+3 5
+%%%%
+2
+%%%% end
+*/
