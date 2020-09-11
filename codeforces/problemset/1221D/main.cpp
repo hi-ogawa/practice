42cf1dca9ec60c627f61930a6872fd706368155f
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-09-11 19:21:06 +0900

    Create codeforces/problemset/1221D

---
+++ codeforces/problemset/1221D/main.cpp
@@ -0,0 +1,96 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utility
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+template<typename T> struct is_container {
+template<typename C> static char test(typename C::const_iterator*); template<typename> static int test(...);
+static bool const value = sizeof(test<T>(nullptr)) == sizeof(char) && !std::is_same<std::string, T>::value;
+};
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i, tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class T1, class T2> istream& operator>>(istream& i, pair<T1, T2>& x) { i >> x.first >> x.second; return i; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+istream& operator>>(istream& i, T& x) { for (auto& y : x) { i >> y; } return i; }
+}
+
+// Debugging
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dbg(X) do { if (DEBUG) { cout << #X ": " << (X) << endl << flush; } } while (0)
+#define dbg2(X) do { if (DEBUG) { cout << #X ":" << endl; for (auto& __x : (X)) { cout << __x << endl << flush; } } } while (0)
+#define dbgv(...) do { if (DEBUG) { cout << "(" #__VA_ARGS__ "): " << make_tuple(__VA_ARGS__) << endl << flush; } } while (0)
+namespace std {
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { o << tie(x.first, x.second); return o; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+}
+
+// Main
+void mainCase() {
+  int n;
+  cin >> n;
+  vector<array<int, 2>> ls(n);
+  cin >> ls;
+
+  //
+  // PROP. at most two increments for each board
+  //
+  ll kInf = 1LL << 60;
+  vector<array<ll, 3>> dp(n, {kInf, kInf, kInf});
+  FOR(j, 0, 3) { dp[0][j] = j * ls[0][1]; }
+  FOR(i, 1, n) {
+    auto [a, b] = ls[i];
+    auto [aa, _bb] = ls[i - 1];
+    FOR(j0, 0, 3) {
+      FOR(j1, 0, 3) {
+        if (a + j1 == aa + j0) { continue; }
+        dp[i][j1] = min(dp[i][j1], dp[i - 1][j0] + j1 * b);
+      }
+    }
+  }
+  dbg(dp);
+
+  ll res = *min_element(ALL(dp[n - 1]));
+  cout << res << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin >> t;
+  FOR(i, 0, t) { mainCase(); }
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/1221D/main.cpp --check
+
+%%%% begin
+3
+3
+2 4
+2 1
+3 5
+3
+2 3
+2 10
+2 6
+4
+1 7
+3 3
+2 6
+1000000000 2
+%%%%
+2
+9
+0
+%%%% end
+*/
