b27268d3a2098d29152d04679c4c9941fb4752cd
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-09-13 17:51:31 +0900

    Create codeforces/problemset/1396B

---
+++ codeforces/problemset/1396B/main.cpp
@@ -0,0 +1,82 @@
+// AFTER EDITORIAL, AC
+
+//
+// CLAIM.
+//   Game state parity
+//     B --> A <-> A
+//     |
+//     C
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utility
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+template<typename T> struct is_container {
+template<typename C> static char test(typename C::const_iterator*); template<typename> static int test(...);
+static bool const value = sizeof(test<T>(nullptr)) == sizeof(char) && !std::is_same<std::string, T>::value;
+};
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i, tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class T1, class T2> istream& operator>>(istream& i, pair<T1, T2>& x) { i >> x.first >> x.second; return i; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+istream& operator>>(istream& i, T& x) { for (auto& y : x) { i >> y; } return i; }
+}
+
+// Debugging
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dbg(X) do { if (DEBUG) { cout << #X ": " << (X) << endl << flush; } } while (0)
+#define dbg2(X) do { if (DEBUG) { cout << #X ":" << endl; for (auto& __x : (X)) { cout << __x << endl << flush; } } } while (0)
+#define dbgv(...) do { if (DEBUG) { cout << "(" #__VA_ARGS__ "): " << make_tuple(__VA_ARGS__) << endl << flush; } } while (0)
+namespace std {
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { o << tie(x.first, x.second); return o; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+}
+
+// Main
+void mainCase() {
+  int n;
+  cin >> n;
+  vector<int> ls(n);
+  cin >> ls;
+
+  int total = accumulate(ALL(ls), 0);
+  bool big = 0;
+  for (auto x : ls) {
+    if (x > total / 2) { big = 1; break; }
+  }
+
+  bool win_2nd = !big && (total % 2 == 0);
+  cout << (win_2nd ? "HL" : "T") << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin >> t;
+  FOR(i, 0, t) { mainCase(); }
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/1396B/main.cpp --check
+
+%%%% begin
+2
+1
+2
+2
+1 1
+%%%%
+T
+HL
+%%%% end
+*/
