49cac7ffd40c7ff0c9186e7f8bd440d5f4b15e4c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-09-16 21:49:37 +0900

    Create codeforces/problemset/1296E1

---
+++ codeforces/problemset/1296E1/main.cpp
@@ -0,0 +1,125 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Utility
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+template<typename T> struct is_container {
+template<typename C> static char test(typename C::const_iterator*); template<typename> static int test(...);
+static bool const value = sizeof(test<T>(nullptr)) == sizeof(char) && !std::is_same<std::string, T>::value;
+};
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i, tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class T1, class T2> istream& operator>>(istream& i, pair<T1, T2>& x) { i >> x.first >> x.second; return i; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+istream& operator>>(istream& i, T& x) { for (auto& y : x) { i >> y; } return i; }
+}
+
+// Debugging
+#ifndef DEBUG
+#define DEBUG 0
+#endif
+#define dbg(X) do { if (DEBUG) { cout << #X ": " << (X) << endl << flush; } } while (0)
+#define dbg2(X) do { if (DEBUG) { cout << #X ":" << endl; for (auto& __x : (X)) { cout << __x << endl << flush; } } } while (0)
+#define dbgv(...) do { if (DEBUG) { cout << "(" #__VA_ARGS__ "): " << make_tuple(__VA_ARGS__) << endl << flush; } } while (0)
+namespace std {
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; apply([&](auto&& y, auto&&... ys){ o << y; ((o << ", " << ys), ...); }, x); o << ")"; return o; }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { o << tie(x.first, x.second); return o; }
+template<class T, enable_if_t<is_container<T>::value, int> = 0>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; for (auto it = x.begin(); it != x.end(); it++) { if (it != x.begin()) { o << ", "; } o << *it; } o << "}"; return o; }
+}
+
+// Main
+void mainCase() {
+  int n; // [1, 200]
+  cin >> n;
+  string s;
+  cin >> s;
+
+  vector<vector<int>> inv(n), invT(n);
+  FOR(i, 0, n) {
+    FOR(j, i + 1, n) {
+      if (s[i] > s[j]) {
+        inv[i].push_back(j);
+        invT[j].push_back(i);
+      }
+    }
+  }
+  dbg(inv);
+  dbg(invT);
+
+  bool ok1 = 1;
+  FOR(i, 0, n) {
+    if (!inv[i].empty() && !invT[i].empty()) {
+      ok1 = 0;
+      break;
+    }
+  }
+  if (!ok1) { cout << "NO" << endl; return; }
+
+  vector<int> res(n, -1);
+  bool ok2 = 1;
+  FOR(i, 0, n) {
+    if (res[i] == -1) { res[i] = 0; }
+
+    for (auto j : inv[i]) {
+      if (res[j] == res[i]) { ok2 = 0; break; }
+      res[j] = !res[i];
+
+      for (auto k : invT[j]) {
+        if (res[k] == res[j]) { ok2 = 0; break; }
+        res[k] = !res[j];
+      }
+    }
+  }
+  assert(ok2);
+
+  cout << "YES" << endl;
+  FOR(i, 0, n) { cout << res[i]; }
+  cout << endl;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/1296E1/main.cpp --check
+
+%%%% begin
+9
+abacbecfd
+%%%%
+YES
+001010101
+%%%% end
+
+%%%% begin
+8
+aaabbcbb
+%%%%
+YES
+01011011
+%%%% end
+
+%%%% begin
+7
+abcdedc
+%%%%
+NO
+%%%% end
+
+%%%% begin
+5
+abcde
+%%%%
+YES
+00000
+%%%% end
+*/
