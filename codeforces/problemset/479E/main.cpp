312f7a40048d87a993eff7f092767177b8f233d6
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-10-01 21:45:21 +0900

    Create codeforces/problemset/479E

---
+++ codeforces/problemset/479E/main.cpp
@@ -0,0 +1,126 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Modulo
+template<ll Modulo>
+struct ModInt {
+  using mint = ModInt;
+  static constexpr ll modulo = Modulo;
+  uint32_t v;
+  ModInt() : v{0} {}
+  template<class T> ModInt(T x) {
+    ll y = (ll)x % modulo;
+    if (y < 0) { y += modulo; }
+    v = y;
+  }
+  friend istream& operator>>(istream& istr,       mint& self) { return istr >> self.v; }
+  friend ostream& operator<<(ostream& ostr, const mint& self) { return ostr << self.v; }
+  mint& operator+=(const mint& y) { v += y.v; while (v >= modulo) { v -= modulo; }; return *this; }
+  mint& operator-=(const mint& y) { return *this += (modulo - y.v); }
+  mint& operator*=(const mint& y) { v = (ll)v * y.v % modulo; return *this; }
+  mint& operator/=(const mint& y) { return *this *= y.inv(); }
+  friend mint operator+(const mint& x, const mint& y) { return mint(x) += y; }
+  friend mint operator-(const mint& x, const mint& y) { return mint(x) -= y; }
+  friend mint operator*(const mint& x, const mint& y) { return mint(x) *= y; }
+  friend mint operator/(const mint& x, const mint& y) { return mint(x) /= y; }
+  friend bool operator==(const mint& x, const mint& y) { return x.v == y.v; }
+  friend bool operator!=(const mint& x, const mint& y) { return x.v != y.v; }
+  mint inv() const { return pow(modulo - 2); }
+  mint pow(ll e) const {
+    mint x = *this, res = 1;
+    while (e > 0) {
+      if (e & 1) { res *= x; }
+      e >>= 1; x *= x;
+    }
+    return res;
+  }
+};
+
+using mint = ModInt<(ll)1e9 + 7>;
+
+// Main
+void mainCase() {
+  int n, a, b, k; // [1, 5000]
+  cin >> n >> a >> b >> k;
+
+  // WLOG, 1 <= a <= b by mirroring
+  if (a > b) {
+    b = n + 1 - b;
+    a = n + 1 - a;
+  }
+  a--; b--;
+
+  vector<mint> dp(b + 1), dp_diff(b + 1);
+  dp[a] = 1;
+  FOR(_, 0, k) {
+    fill(ALL(dp_diff), 0);
+    FOR(i, 0, b) {
+       // Range increment by differece array
+      int d = b - i - 1;
+      int i0 = max(0, i - d), i1 = i + d + 1;
+      dp_diff[i0]    += dp[i];
+      dp_diff[i]     -= dp[i];
+      dp_diff[i + 1] += dp[i];
+      dp_diff[i1]    -= dp[i];
+    }
+    // Apply increments
+    dp = dp_diff;
+    FOR(i, 1, b) { dp[i] += dp[i - 1]; }
+  }
+
+  mint res = accumulate(dp.begin(), dp.begin() + b, (mint)0);
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/479E/main.cpp --check
+
+%%%% begin
+5 2 4 1
+%%%%
+2
+%%%% end
+
+%%%% begin
+5 2 4 2
+%%%%
+2
+%%%% end
+
+%%%% begin
+5 3 4 1
+%%%%
+0
+%%%% end
+*/
