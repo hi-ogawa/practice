ea0f986323a07f26eab36e6c1f97d3bd16943d2a
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-10-20 18:08:59 +0900

    Create codeforces/problemset/1186C

---
+++ codeforces/problemset/1186C/main.cpp
@@ -0,0 +1,79 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (int I = (X); I < (Y); I++)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Main
+void mainCase() {
+  string a, b;
+  cin >> a >> b;
+  int n = a.size(); // [1, 10^6]
+  int m = b.size(); // [1, n]
+
+  // k = diff(b[1:], b[:m-1])
+  int k = 0;
+  FOR(i, 1, m) {
+    k ^= (b[i] != b[i - 1]);
+  }
+
+  int parity = 0;
+  FOR(i, 0, m) {
+    parity ^= (a[i] != b[i]);
+  }
+
+  int res = (parity == 0);
+  FOR(i, 0, n - m) {
+    parity ^= (a[i] != b[0]);
+    parity ^= (a[i + m] != b[m - 1]);
+    parity ^= k;
+    res += (parity == 0);
+  }
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/1186C/main.cpp
+
+%%%% begin
+01100010
+00110
+%%%%
+3
+%%%% end
+
+%%%% begin
+1010111110
+0110
+%%%%
+4
+%%%% end
+*/
