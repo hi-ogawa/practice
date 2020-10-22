0834f3c4185e1fa8233289a75ccf5e90644b6d55
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-10-22 20:17:36 +0900

    Create spoj/DSUBSEQ

---
+++ spoj/DSUBSEQ/main.cpp
@@ -0,0 +1,112 @@
+// AFTER EDITORIAL, AC
+
+// NOTE: https://www.geeksforgeeks.org/count-distinct-subsequences/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (decltype(Y) I = (X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Modulo integer
+template<ll Modulo>
+struct ModInt {
+  using mint = ModInt;
+  static constexpr ll modulo = Modulo;
+  uint32_t v;
+  ModInt() : v{0} {}
+  template<class T, class = enable_if_t<is_integral<T>::value>>
+  ModInt(T x) { ll y = (ll)x % modulo; if (y < 0) { y += modulo; } v = y; }
+  friend istream& operator>>(istream& istr,       mint& self) { return istr >> self.v; }
+  friend ostream& operator<<(ostream& ostr, const mint& self) { return ostr << self.v; }
+  mint& operator+=(const mint& y) { v += y.v; while (v >= modulo) { v -= modulo; }; return *this; }
+  mint& operator-=(const mint& y) { return *this += (modulo - y.v); }
+  mint& operator*=(const mint& y) { v = (ll)v * y.v % modulo; return *this; }
+  mint& operator/=(const mint& y) { return *this *= y.inv(); }
+  friend mint operator+(const mint& x, const mint& y) { return mint(x) += y; }
+  friend mint operator-(const mint& x, const mint& y) { return mint(x) -= y; }
+  friend mint operator*(const mint& x, const mint& y) { return mint(x) *= y; }
+  friend mint operator/(const mint& x, const mint& y) { return mint(x) /= y; }
+  friend bool operator==(const mint& x, const mint& y) { return x.v == y.v; }
+  friend bool operator!=(const mint& x, const mint& y) { return x.v != y.v; }
+  mint operator-() const { return mint() - *this; }
+  mint inv() const { return pow(modulo - 2); }
+  mint pow(ll e) const {
+    mint x = *this, res = 1;
+    while (e > 0) {
+      if (e & 1) { res *= x; }
+      e >>= 1; x *= x;
+    }
+    return res;
+  }
+};
+
+const ll modulo = 1e9 + 7;
+using mint = ModInt<modulo>;
+
+// Main
+void mainCase() {
+  string s; // upper case
+  cin >> s;
+  int n = s.size(); // \le 10^5
+
+  const int k = 26; // A..Z
+  vector<mint> cnts(k);
+
+  mint res = 1;
+  FOR(i, 0, n) {
+    int x = s[i] - 'A';
+    tie(cnts[x], res) = make_tuple(res, 2 * res - cnts[x]);
+  }
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin >> t;
+  FOR(i, 0, t) { mainCase(); }
+  return 0;
+}
+
+/*
+python misc/run.py spoj/DSUBSEQ/main.cpp
+
+%%%% begin
+1
+AAA
+%%%%
+4
+%%%% end
+
+%%%% begin
+3
+AAA
+ABCDEFG
+CODECRAFT
+%%%%
+4
+128
+496
+%%%% end
+*/
