0880b14d2bbd1e28f9cdff92810bde7a7f3f92bb
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-11-01 18:02:10 +0900

    Create cses/mathematics/task1728

---
+++ cses/mathematics/task1728/main.cpp
@@ -0,0 +1,70 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y)(X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+using Real = double;
+
+// Main
+void mainCase() {
+  int n; // [1, 100]
+  cin >> n;
+  vector<int> ls(n); // [1, 100]
+  cin >> ls;
+
+  Real res = 0;
+  FOR(i, 0, n) {
+    int r = ls[i];
+    Real t = 0;
+    FOR(x, 1, r + 1) {
+      FOR(j, 0, i) {
+        // Prob[ inversion (xj, xi) with xi = x ] = ((rj - x) / rj) / ri
+        int s = ls[j];
+        if (s <= x) { continue; }
+        t += Real(s - x) / s;
+      }
+    }
+    res += t / r;
+  }
+  cout << fixed << setprecision(6) << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py cses/mathematics/task1728/main.cpp
+
+%%%% begin
+3
+5 2 7
+%%%%
+1.057143
+%%%% end
+*/
