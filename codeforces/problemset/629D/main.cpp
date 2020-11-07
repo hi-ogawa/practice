9b341f9f218d76ee386d12797cfa734f0a78223c
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-11-07 21:00:00 +0900

    Create codeforces/problemset/629D

---
+++ codeforces/problemset/629D/main.cpp
@@ -0,0 +1,97 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y)(X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+using Real = double;
+const Real kPi = acos(Real(-1));
+
+// Main
+void mainCase() {
+  int n; // [1, 10^5]
+  cin >> n;
+  vector<array<Real, 2>> ls(n); // [1, 10^4]
+  cin >> ls;
+
+  vector<Real> vs(n);
+  FOR(i, 0, n) {
+    auto [r, h] = ls[i];
+    vs[i] = kPi * r * r * h; // TODO: Real factor only comes from this "pi", so this is essentially an integer problem...
+  }
+  dbg(vs);
+
+  // Max sum increasing subsequence
+  set<array<Real, 2>> dp;
+  FOR(i, 0, n) {
+    dbg(dp);
+    auto v = vs[i];
+    dbg(i, v);
+    auto it = dp.lower_bound({v, 0});
+    Real s = v;
+    if (it != dp.begin()) {
+      s += (*prev(it))[1];
+    }
+    while (it != dp.end()) {
+      auto [v2, s2] = *it;
+      assert(v <= v2);
+      if (s2 > s) { break; }
+      it = dp.erase(it);
+    }
+    dp.insert({v, s});
+  }
+  dbg(dp);
+
+  auto [res1, res2] = *--dp.end();
+  cout << fixed << setprecision(10) << res2 << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/problemset/629D/main.cpp
+
+%%%% begin
+2
+100 30
+40 10
+%%%%
+942477.796077000
+%%%% end
+
+%%%% begin
+4
+1 1
+9 7
+1 4
+10 7
+%%%%
+3983.539484752
+%%%% end
+*/
