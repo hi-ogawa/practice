19309c3c36a8ff0ffd83f9107f89415a6e32deb8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-11-20 16:43:19 +0900

    Create codeforces/contest/1452

---
+++ codeforces/contest/1452/b/main.cpp
@@ -0,0 +1,79 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y)(X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Main
+void mainCase() {
+  int n; // [2, 10^5]
+  cin >> n;
+  vector<ll> ls(n); // [0, 10^9]
+  cin >> ls;
+
+  ll m = *max_element(ALL(ls));
+  ll s = accumulate(ALL(ls), (ll)0);
+
+  auto divCeil = [](ll x, ll y) -> ll { return (x + (y - 1)) / y; };
+
+  //
+  // PROP.
+  //   min k s.t.
+  //     k >= m
+  //     k (n - 1) >= s
+  //
+  //   Such k is minimul possible distribution.
+  //
+
+  ll k = max(m, divCeil(s, n - 1));
+  ll res = k * (n - 1) - s;
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  int t;
+  cin >> t;
+  FOR(i, 0, t) { mainCase(); }
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/contest/1452/b/main.cpp
+
+%%%% begin
+3
+3
+3 2 2
+4
+2 2 3 2
+3
+0 3 0
+%%%%
+1
+0
+3
+%%%% end
+*/
---
+++ codeforces/contest/1452/d/main.cpp
@@ -0,0 +1,133 @@
+// CONTEST, AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y)(X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Modulo integer
+template<ll Modulo>
+struct ModInt {
+  using mint = ModInt;
+  static constexpr ll modulo = Modulo;
+  uint32_t v;
+  ModInt() : v{0} {}
+  template<class T, class = enable_if_t<is_integral_v<T>>>
+  ModInt(T x) { ll y = (ll)x % modulo; if (y < 0) { y += modulo; } v = y; }
+  friend istream& operator>>(istream& istr,       mint& self) { return istr >> self.v; }
+  friend ostream& operator<<(ostream& ostr, const mint& self) { return ostr << self.v; }
+  mint& operator+=(const mint& y) { v += y.v; while (v >= modulo) { v -= modulo; }; return *this; }
+  mint& operator-=(const mint& y) { return *this += (modulo - y.v); }
+  mint& operator*=(const mint& y) { v = (ll)v * y.v % modulo; return *this; }
+  mint& operator/=(const mint& y) { return *this *= y.inv(); }
+  friend mint operator+(const mint& x, const mint& y) { return mint(x) += y; }
+  friend mint operator-(const mint& x, const mint& y) { return mint(x) -= y; }
+  friend mint operator*(const mint& x, const mint& y) { return mint(x) *= y; }
+  friend mint operator/(const mint& x, const mint& y) { return mint(x) /= y; }
+  friend bool operator==(const mint& x, const mint& y) { return x.v == y.v; }
+  friend bool operator!=(const mint& x, const mint& y) { return x.v != y.v; }
+  mint operator-() const { return mint() - *this; }
+  mint inv() const { return pow(modulo - 2); }
+  mint pow(ll e) const {
+    mint x = *this, res = 1;
+    while (e > 0) {
+      if (e & 1) { res *= x; }
+      e >>= 1; x *= x;
+    }
+    return res;
+  }
+};
+
+const ll modulo = 998244353;
+using mint = ModInt<modulo>;
+
+// Main
+void mainCase() {
+  int n; // [1, 2 x 10^5]
+  cin >> n;
+
+  //
+  // PROP.
+  //   f(n) = ∑_{k : 1, 3, ...} 1 / 2^k f(n - k)
+  //   f(0) = 1
+  //
+  //   Thus, writing g(n) = 2^n f(n), we have,
+  //
+  //     2^n f(n) = ∑ 2^(n - k) f(n - k)
+  //     g(n) = g(n - 1) + g(n - 3) + ...
+  //     g(0) = 1
+  //
+
+  vector<mint> g(n + 1);
+  mint even = 0;
+  mint odd = 0;
+  g[0] = even = 1;
+  FOR(i, 1, n + 1) {
+    if (i % 2) {
+      g[i] = even;
+      odd += g[i];
+    } else {
+      g[i] = odd;
+      even += g[i];
+    }
+    dbg(i, g[i], mint(2).pow(i));
+  }
+
+  mint res = g[n] / mint(2).pow(n);
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py codeforces/contest/1452/d/main.cpp
+
+%%%% begin
+2
+%%%%
+748683265
+%%%% end
+
+%%%% begin
+3
+%%%%
+748683265
+%%%% end
+
+%%%% begin
+5
+%%%%
+842268673
+%%%% end
+
+%%%% begin
+200000
+%%%%
+202370013
+%%%% end
+*/
