de4cbe4ea1b5070bad47ba45ec14c864fa9070f5
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2020-11-27 14:16:38 +0900

    Update cses/sorting_and_searching/task1629

---
+++ cses/sorting_and_searching/task1629/main_v2.cpp
@@ -0,0 +1,67 @@
+// AC
+
+// NOTE: "dp_max" of main.cpp is always achieved by "first chain".
+//       i.e. finishing time greedy is optimal.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y)(X); I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cout << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cout << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Main
+void mainCase() {
+  int n; // [1, 2 x 10^5]
+  cin >> n;
+  vector<array<int, 2>> ls(n); // [1, 10^9]
+  cin >> ls;
+  sort(ALL(ls), [](auto x, auto y) { return x[1] < y[1]; });
+  int res = 0;
+  int t = 0;
+  for (auto [l, r] : ls) {
+    if (t <= l) {
+      t = r;
+      res++;
+    }
+  }
+  cout << res << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py cses/sorting_and_searching/task1629/main_v2.cpp
+
+%%%% begin
+3
+3 5
+4 9
+5 8
+%%%%
+2
+%%%% end
+*/
