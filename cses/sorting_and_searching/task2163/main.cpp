87646f7587497e0db05926b55042b348b89925c1
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2021-12-28 17:14:42 +0900

    feat: add cses/sorting_and_searching/task2163/main.cpp

---
+++ cses/sorting_and_searching/task2163/main.cpp
@@ -0,0 +1,72 @@
+// AC
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ALL(X) (X).begin(), (X).end()
+#define FOR(I, X, Y) for (auto I = decltype(Y){X}; I < (Y); ++I)
+using ll = int64_t;
+using ull = uint64_t;
+namespace std {
+template<class ...Ts>        istream& operator>>(istream& i,       tuple<Ts...>& x) { apply([&](auto&&... ys){ ((i >> ys), ...); }, x); return i; }
+template<class ...Ts>        ostream& operator<<(ostream& o, const tuple<Ts...>& x) { o << "("; auto s = ""; apply([&](auto&&... y){ ((o << s << y, s = ", "), ...); }, x); return o << ")"; }
+template<class T1, class T2> istream& operator>>(istream& i,       pair<T1, T2>& x) { return i >> tie(x.first, x.second); }
+template<class T1, class T2> ostream& operator<<(ostream& o, const pair<T1, T2>& x) { return o << tie(x.first, x.second); }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+istream& operator>>(istream& i,       T& x) { for (auto& y : x) { i >> y; } return i; }
+template<class T, class = decltype(begin(declval<T>())), class = enable_if_t<!is_same<T, string>::value>>
+ostream& operator<<(ostream& o, const T& x) { o << "{"; auto s = ""; for (auto& y : x) { o << s << y; s = ", "; } return o << "}"; }
+}
+
+#ifdef DEBUG
+#define dbg(...) do { cerr << #__VA_ARGS__ ": " << make_tuple(__VA_ARGS__) << endl; } while (0)
+#define dbg2(X) do { cerr << #X ":\n"; for (auto& __x : (X)) { cout << __x << endl; } } while (0)
+#else
+#define dbg(...)
+#define dbg2(X)
+#endif
+
+// Order statistics tree
+#include <ext/pb_ds/assoc_container.hpp>
+template<class Key, class Compare = less<Key>>
+using ordered_set = __gnu_pbds::tree<
+  Key, __gnu_pbds::null_type, Compare,
+  __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update>;
+
+// Main
+void mainCase() {
+  int n, k;
+  cin >> n >> k;
+
+  // Simulate the operations
+
+  ordered_set<int> ls;
+  for (int i = 0; i < n; ++i) {
+    ls.insert(i);
+  }
+
+  int x = 0;
+  for (int m = n; m > 0; m--) {
+    int i = ls.order_of_key(x);
+    auto y_it = ls.find_by_order((i + k) % m);
+    cout << (*y_it + 1) << " \n"[m == 1];
+    x = *ls.find_by_order((i + k + 1) % m);
+    ls.erase(y_it);
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0); cin.tie(0);
+  mainCase();
+  return 0;
+}
+
+/*
+python misc/run.py cses/sorting_and_searching/task2163/main.cpp
+
+%%%% begin
+7 2
+%%%%
+3 6 2 7 5 1 4
+%%%% end
+*/
