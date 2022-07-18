fd234bb5f7a5123804a6c4d4b68e44c5221e1a2b
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-07-18 19:12:41 +0900

    feat: add atcoder/abc260

---
+++ atcoder/abc260/a/main.rs
@@ -0,0 +1,66 @@
+// WIP
+
+use std::{collections::BTreeMap, fmt::Debug, io, str::FromStr};
+
+fn main() -> io::Result<()> {
+    let tokens: Vec<String> = read_tokens()?;
+    let s = &tokens[0];
+    let mut counts: BTreeMap<char, usize> = BTreeMap::new();
+    for c in s.chars() {
+        *counts.entry(c).or_insert(0) += 1;
+    }
+    let mut done = false;
+    for (&c, &count) in counts.iter() {
+        if count == 1 {
+            done = true;
+            println!("{}", c);
+            break;
+        }
+    }
+    if !done {
+        println!("{}", -1);
+    }
+    Ok(())
+}
+
+/*
+python misc/run.py atcoder/abc260/a/main.rs
+
+%%%% begin
+pop
+%%%%
+o
+%%%% end
+
+%%%% begin
+abc
+%%%%
+a
+%%%% end
+
+%%%% begin
+xxx
+%%%%
+-1
+%%%% end
+*/
+
+//
+// io
+//
+
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
---
+++ atcoder/abc260/b/main.rs
@@ -0,0 +1,131 @@
+// AC
+
+use std::{collections::BTreeSet, fmt::Debug, io, str::FromStr};
+
+fn to_order(values: Vec<i64>) -> Vec<usize> {
+    let n = values.len();
+    let mut result: Vec<usize> = (0..n).collect();
+    result.sort_by_key(|&i| -&values[i]);
+    result
+}
+
+fn main() -> io::Result<()> {
+    let line: Vec<usize> = read_tokens()?;
+    let [n, x, y, z] = [line[0], line[1], line[2], line[3]];
+    let a_vec: Vec<i64> = read_tokens().unwrap();
+    let b_vec: Vec<i64> = read_tokens().unwrap();
+    let c_vec: Vec<i64> = (0..n).map(|i| a_vec[i] + b_vec[i]).collect();
+    let a_order = to_order(a_vec);
+    let b_order = to_order(b_vec);
+    let c_order = to_order(c_vec);
+
+    let mut result: Vec<usize> = Vec::new();
+    let mut done: BTreeSet<usize> = BTreeSet::new();
+    {
+        let mut x_acc = 0;
+        for i in a_order {
+            if x_acc >= x {
+                break;
+            }
+            if !done.contains(&i) {
+                result.push(i);
+                done.insert(i);
+                x_acc += 1;
+            }
+        }
+    }
+    {
+        let mut y_acc = 0;
+        for i in b_order {
+            if y_acc >= y {
+                break;
+            }
+            if !done.contains(&i) {
+                result.push(i);
+                done.insert(i);
+                y_acc += 1;
+            }
+        }
+    }
+    {
+        let mut z_acc = 0;
+        for i in c_order {
+            if z_acc >= z {
+                break;
+            }
+            if !done.contains(&i) {
+                result.push(i);
+                done.insert(i);
+                z_acc += 1;
+            }
+        }
+    }
+
+    result.sort();
+    for i in result {
+        println!("{}", i + 1);
+    }
+    Ok(())
+}
+
+/*
+python misc/run.py atcoder/abc260/b/main.rs
+
+%%%% begin
+6 1 0 2
+80 60 80 60 70 70
+40 20 50 90 90 80
+%%%%
+1
+4
+5
+%%%% end
+
+%%%% begin
+5 2 1 2
+0 100 0 100 0
+0 0 100 100 0
+%%%%
+1
+2
+3
+4
+5
+%%%% end
+
+%%%% begin
+15 4 3 2
+30 65 20 95 100 45 70 85 20 35 95 50 40 15 85
+0 25 45 35 65 70 80 90 40 55 20 20 45 75 100
+%%%%
+2
+4
+5
+6
+7
+8
+11
+14
+15
+%%%% end
+*/
+
+//
+// io
+//
+
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
