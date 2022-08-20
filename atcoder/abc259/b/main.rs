b7c4742e76de56bce4bada563c32e7cfddf93a08
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-08-20 14:57:15 +0900

    feat: add atcoder/abc259/{b,c}

---
+++ atcoder/abc259/b/main.rs
@@ -0,0 +1,80 @@
+// AC
+
+// https://atcoder.jp/contests/abc259/tasks/abc259_b
+
+use std::{f64::consts::PI, fmt::Debug, io, str::FromStr};
+
+fn main_case() -> io::Result<()> {
+    let (a, b, d) = read_tokens::<f64>().map(|v| (v[0], v[1], v[2]))?;
+
+    fn multiply(m: [[f64; 2]; 2], v: [f64; 2]) -> [f64; 2] {
+        [
+            m[0][0] * v[0] + m[0][1] * v[1],
+            m[1][0] * v[0] + m[1][1] * v[1],
+        ]
+    }
+
+    let t = d / 180.0 * PI;
+    let result = multiply([[t.cos(), -t.sin()], [t.sin(), t.cos()]], [a, b]);
+    println!("{} {}", result[0], result[1]);
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    main_case()
+}
+
+/*
+python misc/run.py atcoder/abc259/b/main.rs
+
+%%%% begin
+2 2 180
+%%%%
+-2 -2
+%%%% end
+
+%%%% begin
+5 0 120
+%%%%
+-2.49999999999999911182 4.33012701892219364908
+%%%% end
+
+%%%% begin
+0 0 11
+%%%%
+0.00000000000000000000 0.00000000000000000000
+%%%% end
+
+%%%% begin
+15 5 360
+%%%%
+15.00000000000000177636 4.99999999999999555911
+%%%% end
+
+%%%% begin
+-505 191 278
+%%%%
+118.85878514480690171240 526.66743699786547949770
+%%%% end
+*/
+
+//
+// utils
+//
+
+#[allow(dead_code)]
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
---
+++ atcoder/abc259/c/main.rs
@@ -0,0 +1,88 @@
+// AC
+
+// https://atcoder.jp/contests/abc259/tasks/abc259_c
+
+use std::{fmt::Debug, io, str::FromStr};
+
+fn main_case() -> io::Result<()> {
+    // length [2, 2 x 10^5]
+    let s1 = read_tokens::<String>()?[0].clone();
+    let s2 = read_tokens::<String>()?[0].clone();
+
+    // split into components
+    fn split(s: &String) -> Vec<(char, usize)> {
+        let s: Vec<char> = s.chars().collect();
+        if s.len() == 0 {
+            return vec![];
+        }
+        let mut result = vec![(s[0], 1)];
+        for i in 1..s.len() {
+            if s[i] != s[i - 1] {
+                result.push((s[i], 1));
+            } else {
+                result.last_mut().unwrap().1 += 1;
+            }
+        }
+        result
+    }
+
+    let components1 = split(&s1);
+    let components2 = split(&s2);
+
+    if components1.len() == components2.len() {
+        if (0..components1.len()).all(|i| {
+            let (c1, d1) = components1[i];
+            let (c2, d2) = components2[i];
+            c1 == c2 && (d1 == d2 || (2 <= d1 && d1 <= d2))
+        }) {
+            println!("Yes");
+            return Ok(());
+        }
+    }
+
+    println!("No");
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    main_case()
+}
+
+/*
+python misc/run.py atcoder/abc259/c/main.rs
+
+%%%% begin
+abbaac
+abbbbaaac
+%%%%
+Yes
+%%%% end
+
+%%%% begin
+xyzz
+xyyzz
+%%%%
+No
+%%%% end
+*/
+
+//
+// utils
+//
+
+#[allow(dead_code)]
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
