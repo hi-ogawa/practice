f2fd4a9073fe653d709014049d66cad97424c168
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-11-20 19:56:51 +0900

    chore: update misc/templates/lib.rs (modulo integer)

--- misc/templates/lib.rs
+++ misc/templates/lib.rs
@@ -2,5 +2,84 @@
 // fenwick tree
 // segment tree
 // dsu
-// modulo integer
 // bit iteration
+
+//
+// modulo integer (e.g. atcoder/abc262/e/main.rs)
+//
+
+const MODULO: usize = 998244353;
+
+#[derive(PartialEq, Eq, Debug, Clone, Copy)]
+struct Mint(usize);
+
+impl Mint {
+    fn pow(&self, mut e: usize) -> Self {
+        let mut x = *self;
+        let mut y = Mint(1);
+        while e > 0 {
+            if e & 1 > 0 {
+                y = y * x;
+            }
+            x = x * x;
+            e >>= 1;
+        }
+        y
+    }
+
+    fn inv(&self) -> Self {
+        self.pow(MODULO - 2)
+    }
+}
+
+impl std::ops::AddAssign for Mint {
+    fn add_assign(&mut self, rhs: Self) {
+        self.0 = (self.0 + rhs.0) % MODULO;
+    }
+}
+
+impl std::ops::MulAssign for Mint {
+    fn mul_assign(&mut self, rhs: Self) {
+        self.0 = (self.0 * rhs.0) % MODULO;
+    }
+}
+
+impl std::ops::DivAssign for Mint {
+    fn div_assign(&mut self, rhs: Self) {
+        *self *= rhs.inv();
+    }
+}
+
+impl std::ops::Add for Mint {
+    type Output = Self;
+    fn add(mut self, rhs: Self) -> Self::Output {
+        self += rhs;
+        self
+    }
+}
+
+impl std::ops::Mul for Mint {
+    type Output = Self;
+    fn mul(mut self, rhs: Self) -> Self::Output {
+        self *= rhs;
+        self
+    }
+}
+
+impl std::ops::Div for Mint {
+    type Output = Self;
+    fn div(mut self, rhs: Self) -> Self::Output {
+        self /= rhs;
+        self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mint_inverse() {
+        assert_eq!(Mint(18181) / Mint(18181), Mint(1));
+    }
+}
