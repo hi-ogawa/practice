d9e3593310bd43d0ddc89b2be67a6e29eb6acb11
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2022-12-10 14:58:52 +0900

    feat: add atcoder/abc279/e/main.rs (AC)

---
+++ atcoder/abc279/e/main.rs
@@ -0,0 +1,106 @@
+// AC
+
+// https://atcoder.jp/contests/abc279/tasks/abc279_e
+
+use std::{fmt::Debug, io, str::FromStr};
+
+fn main_case() -> io::Result<()> {
+    // n, m ~ 10^5
+    let (n, m) = read_tokens::<usize>().map(|v| (v[0], v[1]))?;
+
+    // A \in [0, n - 2]
+    let ls: Vec<usize> = read_tokens()?;
+    let ls: Vec<usize> = ls.iter().map(|&x| x - 1).collect();
+
+    // precompute reverse swaps
+    let mut rev_perm: Vec<usize> = (0..n).collect();
+    let mut rev_perm_inv: Vec<usize> = (0..n).collect();
+    for i in 0..m {
+        let x = ls[i];
+        rev_perm_inv.swap(rev_perm[x], rev_perm[x + 1]);
+        rev_perm.swap(x, x + 1);
+    }
+
+    // incrementally compute
+    let mut perm: Vec<usize> = (0..n).collect();
+    let mut perm_inv: Vec<usize> = (0..n).collect();
+    for i in 0..m {
+        let x = ls[i];
+        // revert reverse swaps
+        rev_perm_inv.swap(x, x + 1);
+
+        // merge forawrd/reverse mapping
+        let result = rev_perm_inv[perm_inv[0]] + 1;
+        println!("{}", result);
+
+        // apply forward swap
+        perm_inv.swap(perm[x], perm[x + 1]);
+        perm.swap(x, x + 1);
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    main_case()
+}
+
+/*
+python misc/run.py atcoder/abc279/e/main.rs
+
+%%%% begin
+5 4
+1 2 3 2
+%%%%
+1
+3
+2
+4
+%%%% end
+
+%%%% begin
+3 3
+2 2 2
+%%%%
+1
+1
+1
+%%%% end
+
+%%%% begin
+10 10
+1 1 1 9 4 4 2 1 3 3
+%%%%
+2
+2
+2
+3
+3
+3
+1
+3
+4
+4
+%%%% end
+*/
+
+//
+// utils
+//
+
+#[allow(dead_code)]
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
---
+++ atcoder/abc279/e/main_v2.rs
@@ -0,0 +1,128 @@
+// AC
+
+// https://atcoder.jp/contests/abc279/tasks/abc279_e
+
+use std::{fmt::Debug, io, str::FromStr};
+
+fn main_case() -> io::Result<()> {
+    // n, m ~ 10^5
+    let (n, m) = read_tokens::<usize>().map(|v| (v[0], v[1]))?;
+
+    // A \in [0, n - 2]
+    let ls: Vec<usize> = read_tokens()?;
+    let ls: Vec<usize> = ls.iter().map(|&x| x - 1).collect();
+
+    // precompute all permutation
+    let mut perm_suffix = Permutaton::new(n);
+    for i in 0..m {
+        perm_suffix.swap(ls[i], ls[i] + 1);
+    }
+
+    // incrementally compute
+    let mut perm = Permutaton::new(n);
+    for i in 0..m {
+        // revert prefix swap
+        perm_suffix.swap_pre(ls[i], ls[i] + 1);
+
+        // merge prefix/suffix mapping
+        let result = perm_suffix.map[perm.map[0]] + 1;
+        println!("{}", result);
+
+        // apply prefix swap
+        perm.swap(ls[i], ls[i] + 1);
+    }
+
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    main_case()
+}
+
+//
+// permutation
+//
+
+struct Permutaton {
+    map: Vec<usize>,
+    inv_map: Vec<usize>, // i.e. final order of (0, .., n - 1) when the mapping is applied
+}
+
+impl Permutaton {
+    fn new(n: usize) -> Self {
+        Self {
+            inv_map: (0..n).collect(),
+            map: (0..n).collect(),
+        }
+    }
+
+    fn swap(&mut self, i: usize, j: usize) {
+        self.map.swap(self.inv_map[i], self.inv_map[j]);
+        self.inv_map.swap(i, j);
+    }
+
+    fn swap_pre(&mut self, i: usize, j: usize) {
+        self.inv_map.swap(self.map[i], self.map[j]);
+        self.map.swap(i, j);
+    }
+}
+
+/*
+python misc/run.py atcoder/abc279/e/main_v2.rs
+
+%%%% begin
+5 4
+1 2 3 2
+%%%%
+1
+3
+2
+4
+%%%% end
+
+%%%% begin
+3 3
+2 2 2
+%%%%
+1
+1
+1
+%%%% end
+
+%%%% begin
+10 10
+1 1 1 9 4 4 2 1 3 3
+%%%%
+2
+2
+2
+3
+3
+3
+1
+3
+4
+4
+%%%% end
+*/
+
+//
+// utils
+//
+
+#[allow(dead_code)]
+fn read_tokens<F: FromStr>() -> io::Result<Vec<F>>
+where
+    <F as FromStr>::Err: Debug,
+{
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)?;
+    let mut result: Vec<F> = Vec::new();
+    for token in line.trim().split(" ") {
+        let value: F = token
+            .parse()
+            .map_err(|e| io::Error::new(io::ErrorKind::Other, format!("{:?}", e)))?;
+        result.push(value);
+    }
+    Ok(result)
+}
